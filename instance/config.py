57053f7c8413ead2f7eba6a09972f82833b3434b
yerman21 <danyaguirre@upeu.edu.pe>
2019-09-10 02:49:31 -0500

    vamos

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,3 @@
 # instance/config.py
 """cargando las configuraciones de instance/config.py para el local"""
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://db_user:@localhost/planificacionCompras"
-
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/planificacionCompras"
